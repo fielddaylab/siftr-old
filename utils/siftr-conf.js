0046d988f3b690cba2d9a2ef8ee6a5d98b5c7e57
Jazmyn R <jazmynrussell@dyn-206-35.vpn.wisc.edu>
2014-08-21 12:34:42 -0500

    Fix minor color problem on desktop

--- utils/siftr-conf.js
+++ utils/siftr-conf.js
@@ -1,22 +1,22 @@
-var TAG_INTRO = "Siftr asks the UW Madison community to photograph and share examples of:";
+var TAG_INTRO = "Participants in Folklore 100 are asked to photograph and share examples of:";
 
 //Short descriptions of the categories that notes within your version of siftr can fall into
 var TAG_DESCRIPTIONS = [
-						"Innovation: What breakthroughs has UW Madison offered the world?",
-						"Stories of the Past: What stories have been hidden by time?",
-						"Madison Culture: What makes our community unique? What do we value?",
-						"Must Do: What experiences should everyone have?",
-						"100 Years from Now: What directions might we head? What decisions are being made?"
+						"Traditions",
+						"Everday Art",
+						"Folk Groups",
+						"Places",
+						"Stories"
 						];
 var CATEGORIES = [
-					"Innovation",
-					"Stories of the Past",
-					"Madison Culture",
-					"Must Do",
-					"100 Years from Now"
-					];
+					"Traditions",
+					"Everday Art",
+					"Folk Groups",
+					"Places",
+					"Stories"
+				];
 
-var ABOUT_SIFTR = "Introducing Siftr, a social photography app that promotes creative discussion about innovation and culture at UW Madison.";
+var ABOUT_SIFTR = "Folklore 100";
 
 
 
