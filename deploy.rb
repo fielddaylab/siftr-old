8553ac40359537beac47520f760e07b8be565396
Michael Tolly <miketolly@gmail.com>
2014-10-17 16:24:09 -0500

    Restored UW siftr; better deploy script
    Now "./deploy.rb" with no args uploads uw siftr to root.

--- deploy.rb
+++ deploy.rb
@@ -11,33 +11,30 @@ def log(s)
   STDERR.puts s
 end
 
-if ARGV.length != 1
+base_dir = '.'
+case ARGV.length
+when 0
+  siftr = 'uw'
+  override_dir = "override/uw"
+  remote_dir = "/httpdocs"
+when 1
+  siftr = ARGV[0]
+  override_dir = "override/#{siftr}"
+  remote_dir = "/httpdocs/#{siftr}"
+else
   log "Usage: #{$0} siftr-name"
   exit 1
 end
-siftr = ARGV[0]
 
-base_dir = '.'
-override_dir = "override/#{siftr}"
-remote_dir = "/httpdocs/#{siftr}"
-
-def rm_rf(sftp, dir)
-  log "Removing directory #{dir}"
-  sftp.dir.entries(dir).each do |ent|
-    next if %w{. ..}.include? ent.name
-    full = "#{dir}/#{ent.name}"
-    if ent.file?
-      sftp.remove! full
-    else
-      rm_rf sftp, full
-    end
-  end
-  sftp.rmdir! dir
+def mkdir_f(sftp, dir)
+  sftp.mkdir! dir
 rescue Net::SFTP::StatusException
-  # if the directory doesn't exist
+  # directory already exists
 end
 
 def upload_rf(sftp, from, to)
+  skip = %w{override .git}
+  return if skip.any? { |s| from.end_with?('/' + s) }
   log "Uploading #{from} to #{to}"
   Dir.entries(from).each do |ent|
     next if %w{. ..}.include? ent
@@ -51,11 +48,7 @@ def upload_rf(sftp, from, to)
       end
       sftp.upload! full_from, full_to
     else
-      begin
-        sftp.mkdir! full_to
-      rescue Net::SFTP::StatusException
-        # dir doesn't already exist
-      end
+      mkdir_f sftp, full_to
       upload_rf sftp, full_from, full_to
     end
   end
@@ -67,14 +60,12 @@ unless File.directory?(override_dir)
 end
 
 Net::SFTP.start(url, username, password: password) do |sftp|
-  log "==> Connected, beginning deploy of #{siftr} siftr."
-  log '==> Removing existing directory...'
-  rm_rf sftp, remote_dir
-  log '==> Making new empty directory...'
-  sftp.mkdir! remote_dir
-  log '==> Uploading base repo...'
-  sftp.upload! base_dir, remote_dir
-  log "==> Uploading #{siftr}-specific overrides..."
+  log " => Connected, beginning deploy of #{siftr} siftr."
+  log " => Ensuring remote dir #{remote_dir} exists..."
+  mkdir_f sftp, remote_dir
+  log ' => Uploading base repo...'
+  upload_rf sftp, base_dir, remote_dir
+  log " => Uploading #{siftr}-specific overrides..."
   upload_rf sftp, override_dir, remote_dir
-  log '==> Done!'
+  log ' => Done!'
 end
---
+++ override/uw/utils/server-conf.js
@@ -0,0 +1,10 @@
+var ROOT_URL = "http://www.arisgames.org";
+var SERVER_URL = ROOT_URL+"/server";
+var FACEBOOK_ID = 419129601530107; //prod game
+
+// var FACEBOOK_ID = 123456; //invalid prod game
+
+var YOI_GAME_ID = 10690; //regular lots of notes
+// var YOI_GAME_ID = 19122; //the network siftr
+// var YOI_GAME_ID = 19147; //folklore 100 siftr
+// var YOI_GAME_ID = 19158; //CFYE Siftr
