7b854ea3e362513f8086c1820133643be0846ac4
Michael Tolly <miketolly@gmail.com>
2014-11-10 16:32:23 -0600

    campuscarts siftr

---
+++ override/campuscarts/utils/server-conf.js
@@ -0,0 +1,7 @@
+var ROOT_URL = "http://www.arisgames.org";
+var SERVER_URL = ROOT_URL+"/server";
+var FACEBOOK_ID = 419129601530107; //prod game
+
+// var FACEBOOK_ID = 123456; //invalid prod game
+
+var YOI_GAME_ID = 20608;
