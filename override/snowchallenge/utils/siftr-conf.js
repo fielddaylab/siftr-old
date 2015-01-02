2e9ee1e4a031ac7cf1458ba33c50209d6d36a919
Michael Tolly <miketolly@gmail.com>
2015-01-02 13:21:43 -0600

    Snow Challenge 2015 Siftr

---
+++ override/snowchallenge/utils/siftr-conf.js
@@ -0,0 +1,9 @@
+var ABOUT_SIFTR = '<a href="http://tadubois.com/Muohta/Snow_Challenge_index.html">See here</a> for some descriptions of the different types of snow.';
+
+// the latitude of the center point of the map
+var MAP_CENTER_LATITUDE = 43.0753;
+
+// the longitude of the center point of the map
+var MAP_CENTER_LONGITUDE = -89.4041;
+
+var MAP_ZOOM_LEVEL = 14;
