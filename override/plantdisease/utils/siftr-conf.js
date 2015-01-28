d7278c3c9bf0999a589a036fcc6a65e24ac8e4e4
Michael Tolly <miketolly@gmail.com>
2015-01-28 12:22:58 -0600

    plant disease siftr; deleted unused audio

---
+++ override/plantdisease/utils/siftr-conf.js
@@ -0,0 +1,9 @@
+var ABOUT_SIFTR = "Dr. Death's Plant Disease Tour";
+
+// the latitude of the center point of the map
+var MAP_CENTER_LATITUDE = 43.0753;
+
+// the longitude of the center point of the map
+var MAP_CENTER_LONGITUDE = -89.4041;
+
+var MAP_ZOOM_LEVEL = 14;
