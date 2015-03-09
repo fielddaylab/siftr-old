d99e37e451fa3a1fae5e0a87930eec4f373a7587
Michael Tolly <miketolly@gmail.com>
2015-03-09 16:36:23 -0500

    fixed some non-pointer-cursor links

--- utils/server-conf.js
+++ utils/server-conf.js
@@ -2,7 +2,6 @@ var FACEBOOK_ID = 419129601530107;
 
 var ROOT_URL = 'http://dev.arisgames.org';
 var SERVER_URL = ROOT_URL+"/server";
-var DEFAULT_SIFTR_ID = 351;
 
 // var ROOT_URL = 'http://localhost';
 // var SERVER_URL = ROOT_URL+"/aris";
