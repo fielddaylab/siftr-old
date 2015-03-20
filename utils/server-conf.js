0baa12a9f2c741d3b9034ff9d0895e58f63c5907
Michael Tolly <miketolly@gmail.com>
2015-03-20 09:24:48 -0500

    added Siftr logo, switched to production Aris server

--- utils/server-conf.js
+++ utils/server-conf.js
@@ -1,6 +1,6 @@
 var FACEBOOK_ID = 419129601530107;
 
-var ROOT_URL = 'http://dev.arisgames.org';
+var ROOT_URL = 'http://arisgames.org';
 var SERVER_URL = ROOT_URL+"/server";
 
 // var ROOT_URL = 'http://localhost';
